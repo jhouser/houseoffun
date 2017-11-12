71294309e4e43f6bb849b2da4e60214d9e11311b
Jake Houser <jakob.houser@gmail.com>
2017-11-12 11:47:33 +0100

    Add migration for Games table

--- houseoffun/houseoffun/models.py
+++ houseoffun/houseoffun/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length = 100, unique = True)
+    abbreviation = models.CharField(max_length = 10)
+    description = models.TextField()
+    game_master = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+    created_at = models.DateField(auto_now_add = True)
--- houseoffun/settings.py
+++ houseoffun/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.core.mail',
-    'houseoffun',
+    'houseoffun.houseoffun',
 ]
 
 MIDDLEWARE = [

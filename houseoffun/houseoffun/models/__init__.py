bbd79cadc93a045fe0f70879fb0664f16f91df95
Jake Houser <jakob.houser@gmail.com>
2017-11-19 13:55:08 +0100

    Add thread model

--- houseoffun/houseoffun/models/__init__.py
+++ houseoffun/houseoffun/models/__init__.py
@@ -1,2 +1,3 @@
 from houseoffun.houseoffun.models.core import *
 from houseoffun.houseoffun.models.games import *
+from houseoffun.houseoffun.models.threads import *
--- houseoffun/houseoffun/models/threads.py
+++ houseoffun/houseoffun/models/threads.py
@@ -1,2 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from houseoffun.houseoffun.models.games import Game
+
+class Thread(models.Model):
+    name = models.CharField(max_length = 255)
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete = models.CASCADE
+    )
+    created_at = models.DateField(auto_now_add = True)
+    text = models.TextField()

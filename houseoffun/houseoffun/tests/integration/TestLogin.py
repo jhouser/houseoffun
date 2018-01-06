a6a62b04a4b0f7069a8f3ce64bd8716fbddce31d
Jake Houser <jakob.houser@gmail.com>
2018-01-06 15:46:04 +0100

    Fix typo in password

--- houseoffun/houseoffun/tests/integration/TestLogin.py
+++ houseoffun/houseoffun/tests/integration/TestLogin.py
@@ -22,6 +22,6 @@ class LoginTest(StaticLiveServerTestCase):
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys('test_user')
         password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('test_password')
+        password_input.send_keys('testpassword')
         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
         self.wait.until(EC.url_changes('%s%s' % (self.live_server_url, '/accounts/login/')))

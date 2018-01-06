86e14948f5ca85f068c175947ae7333492b0ea6b
Jake Houser <jakob.houser@gmail.com>
2018-01-06 12:18:33 +0100

    First Travis CI commit

---
+++ houseoffun/houseoffun/tests/integration/TestLogin.py
@@ -0,0 +1,23 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class LoginTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(LoginTest, cls).setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(LoginTest, cls).tearDownClass()
+
+
+    def test_login(self):
+        self.selenium.get('%s %s' % (self.live_server_url, '/accounts/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('test_user')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('test_password')
+        self.selenium.find_element_by_xpath(' //input[@value="Log in"]').click()

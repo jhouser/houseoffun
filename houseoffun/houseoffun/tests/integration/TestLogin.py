b26eddca05a06a1527d472f27a7711fd0dd12bec
Jake Houser <jakob.houser@gmail.com>
2018-01-06 14:15:49 +0100

    Add print statement of page source

--- houseoffun/houseoffun/tests/integration/TestLogin.py
+++ houseoffun/houseoffun/tests/integration/TestLogin.py
@@ -1,6 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
-from urllib.parse import urljoin
 
 class LoginTest(StaticLiveServerTestCase):
     @classmethod
@@ -14,11 +13,10 @@ class LoginTest(StaticLiveServerTestCase):
         super(LoginTest, cls).tearDownClass()
 
     def test_login(self):
-        #url = urljoin(self.live_server_url, '/accounts/login')
-        self.selenium.get('http://example.com')
-        self.selenium.find_element_by_link_text('More information...')
-        # username_input = self.selenium.find_element_by_name("username")
-        # username_input.send_keys('test_user')
-        # password_input = self.selenium.find_element_by_name("password")
-        # password_input.send_keys('test_password')
-        # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
+        print(self.selenium.page_source)
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('test_user')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('test_password')
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()

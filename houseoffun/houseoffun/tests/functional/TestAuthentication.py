f5d97d77a620b789655ab6dc28c4027f4c42156d
Jake Houser <jakob.houser@gmail.com>
2018-01-07 13:45:53 +0100

    Add forgot password test

--- houseoffun/houseoffun/tests/functional/TestAuthentication.py
+++ houseoffun/houseoffun/tests/functional/TestAuthentication.py
@@ -36,3 +36,12 @@ class AuthenticationTests(BaseStaticLiveServerTestCase):
         logout_button = self.selenium.find_element_by_link_text('Logout')
         logout_button.click()
         self.wait.until(EC.url_to_be('%s%s' % (self.live_server_url, '/accounts/login/')))
+
+    def test_forgot_password(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
+        self.selenium.find_element_by_link_text('Reset it').click()
+        self.wait.until(EC.url_to_be('%s%s' % (self.live_server_url, '/accounts/password/reset/')))
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys(TestHelper.random_string(8) + '@' + TestHelper.random_string(8) + '.com')
+        self.selenium.find_element_by_xpath('//input[@value="Reset password"]').click()
+        self.wait.until(EC.url_to_be('%s%s' % (self.live_server_url, '/accounts/password/reset/done/')))

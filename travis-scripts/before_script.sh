73b93c77adee76639aa77964a3cb19c9130f7658
Jake Houser <jakob.houser@gmail.com>
2018-01-08 23:29:25 +0100

    Collect static for all tests

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -2,8 +2,8 @@
 set -ev
 mysql -e 'create database travis_ci;'
 python manage.py migrate
+python manage.py collectstatic
 if [[ "${TESTFOLDER}" == *"functional"* ]]; then
     sh -e /etc/init.d/xvfb start
     sleep 3 # give xvfb some time to start
-    python manage.py collectstatic
 fi

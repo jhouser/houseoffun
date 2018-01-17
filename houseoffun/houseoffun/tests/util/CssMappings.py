9377f8c92275c43572d5703c1060f85c04cda530
Jake Houser <jakob.houser@gmail.com>
2018-01-17 19:28:29 +0100

    Refactor signup tests to use CSS selector constants

---
+++ houseoffun/houseoffun/tests/util/CssMappings.py
@@ -0,0 +1,16 @@
+"""
+Constants with CSS selector mappings to easily keep track of things on the page
+"""
+
+# Game Links
+SIGNUP_GAME_LINK_ID = "game-view-link-4"
+WITHDRAW_GAME_LINK_ID = "game-view-link-2"
+PROCESS_SIGNUPS_GAME_LINK_ID = "game-view-link-1"
+
+# Game Elements
+SIGNUP_BUTTON_CLASS = "signup-button-signup"
+WITHDRAW_BUTTON_CLASS = "signup-button-withdraw"
+ACCEPT_BUTTON_CLASS = "signup-button-accept"
+REJECT_BUTTON_CLASS = "signup-button-reject"
+SIGNUP_STATUS_TEXT_CLASS = "signup-status"
+
--- houseoffun/houseoffun/tests/util/__init__.py
+++ houseoffun/houseoffun/tests/util/__init__.py
@@ -1,2 +1,3 @@
 from houseoffun.houseoffun.tests.util.BaseStaticLiveServerTestCase import *
 from houseoffun.houseoffun.tests.util.TestHelper import *
+from houseoffun.houseoffun.tests.util.CssMappings import *

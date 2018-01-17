6eb66f3910caf409de09512917bdfbd05e1bc6a8
Jake Houser <jakob.houser@gmail.com>
2018-01-17 19:28:49 +0100

    Refactor import name

--- houseoffun/houseoffun/tests/functional/games/TestSignups.py
+++ houseoffun/houseoffun/tests/functional/games/TestSignups.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support import expected_conditions as ec
 
 from houseoffun.houseoffun.tests.util import BaseStaticLiveServerTestCase
-from houseoffun.houseoffun.tests.util import CssMappings as cm
+from houseoffun.houseoffun.tests.util import CssMappings as css
 
 
 class SignupsTest(BaseStaticLiveServerTestCase):
@@ -10,39 +10,39 @@ class SignupsTest(BaseStaticLiveServerTestCase):
     def test_game_signup(self):
         self.authenticate()
         self.selenium.get('%s%s' % (self.live_server_url, '/games/'))
-        self.selenium.find_element_by_id(cm.SIGNUP_GAME_LINK_ID).click()
+        self.selenium.find_element_by_id(css.SIGNUP_GAME_LINK_ID).click()
         self.wait.until(ec.url_changes('%s%s' % (self.live_server_url, '/games/')))
-        self.selenium.find_element_by_class_name(cm.SIGNUP_BUTTON_CLASS).click()
+        self.selenium.find_element_by_class_name(css.SIGNUP_BUTTON_CLASS).click()
         self.wait.until(ec.url_contains('%s%s' % (self.live_server_url, '/games/view/')))
-        self.selenium.find_element_by_class_name(cm.SIGNUP_STATUS_TEXT_CLASS)
+        self.selenium.find_element_by_class_name(css.SIGNUP_STATUS_TEXT_CLASS)
         assert 'Registered' in self.selenium.page_source
 
     def test_game_withdraw(self):
         self.authenticate()
         self.selenium.get('%s%s' % (self.live_server_url, '/games/'))
-        self.selenium.find_element_by_id(cm.WITHDRAW_GAME_LINK_ID).click()
+        self.selenium.find_element_by_id(css.WITHDRAW_GAME_LINK_ID).click()
         self.wait.until(ec.url_changes('%s%s' % (self.live_server_url, '/games/')))
-        self.selenium.find_element_by_class_name(cm.WITHDRAW_BUTTON_CLASS).click()
+        self.selenium.find_element_by_class_name(css.WITHDRAW_BUTTON_CLASS).click()
         self.wait.until(ec.url_contains('%s%s' % (self.live_server_url, '/games/view/')))
-        self.selenium.find_element_by_class_name(cm.SIGNUP_STATUS_TEXT_CLASS)
+        self.selenium.find_element_by_class_name(css.SIGNUP_STATUS_TEXT_CLASS)
         assert 'Withdrawn' in self.selenium.page_source
 
     def test_game_accept(self):
         self.authenticate()
         self.selenium.get('%s%s' % (self.live_server_url, '/games/'))
-        self.selenium.find_element_by_id(cm.PROCESS_SIGNUPS_GAME_LINK_ID).click()
+        self.selenium.find_element_by_id(css.PROCESS_SIGNUPS_GAME_LINK_ID).click()
         self.wait.until(ec.url_changes('%s%s' % (self.live_server_url, '/games/')))
-        self.selenium.find_element_by_class_name(cm.ACCEPT_BUTTON_CLASS).click()
+        self.selenium.find_element_by_class_name(css.ACCEPT_BUTTON_CLASS).click()
         self.wait.until(ec.url_contains('%s%s' % (self.live_server_url, '/games/view/')))
-        self.selenium.find_element_by_class_name(cm.SIGNUP_STATUS_TEXT_CLASS)
+        self.selenium.find_element_by_class_name(css.SIGNUP_STATUS_TEXT_CLASS)
         assert 'Accepted' in self.selenium.page_source
 
     def test_game_reject(self):
         self.authenticate()
         self.selenium.get('%s%s' % (self.live_server_url, '/games/'))
-        self.selenium.find_element_by_id(cm.PROCESS_SIGNUPS_GAME_LINK_ID).click()
+        self.selenium.find_element_by_id(css.PROCESS_SIGNUPS_GAME_LINK_ID).click()
         self.wait.until(ec.url_changes('%s%s' % (self.live_server_url, '/games/')))
-        self.selenium.find_element_by_class_name(cm.REJECT_BUTTON_CLASS).click()
+        self.selenium.find_element_by_class_name(css.REJECT_BUTTON_CLASS).click()
         self.wait.until(ec.url_contains('%s%s' % (self.live_server_url, '/games/view/')))
-        self.selenium.find_element_by_class_name(cm.SIGNUP_STATUS_TEXT_CLASS)
+        self.selenium.find_element_by_class_name(css.SIGNUP_STATUS_TEXT_CLASS)
         assert 'Rejected' in self.selenium.page_source

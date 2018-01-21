888ea1465818fcc94111275cce2c0402c19efabe
Jake Houser <jakob.houser@gmail.com>
2018-01-21 12:45:13 +0100

    Update script

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -6,5 +6,5 @@ python manage.py migrate
 if [[ "${TESTFOLDER}" != *"unit"* ]]; then
     sh -e /etc/init.d/xvfb start
     sleep 3 # give xvfb some time to start
-    python manage.py collectstatic
+    python manage.py collectstatic --noinput
 fi

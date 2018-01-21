2749b186d6a19fc9f4ec971f18dc21d6843423c6
Jake Houser <jakob.houser@gmail.com>
2018-01-21 12:39:56 +0100

    Add environment variables for Travis

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 mysql -e 'create database travis_ci;'
+mv .env.travis .env
 python manage.py migrate
 if [[ "${TESTFOLDER}" != *"unit"* ]]; then
     sh -e /etc/init.d/xvfb start

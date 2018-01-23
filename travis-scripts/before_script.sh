f549cf2c1e1cecc3e755f422c3283a73c7356653
Jake Houser <jakob.houser@gmail.com>
2018-01-23 21:33:36 +0100

    Remove collectstatic from travis

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -6,5 +6,4 @@ python manage.py migrate
 if [[ "${TESTFOLDER}" != *"unit"* ]]; then
     sh -e /etc/init.d/xvfb start
     sleep 3 # give xvfb some time to start
-    python manage.py collectstatic --noinput
 fi

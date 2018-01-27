c009663f2f70f52e71aa1c5937f4308b455ca093
Jake Houser <jakob.houser@gmail.com>
2018-01-27 13:23:39 +0100

    Update is_authenticated

--- actionphase/app/views/core.py
+++ actionphase/app/views/core.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    name = request.user.email if request.user.is_authenticated() else 'world'
+    name = request.user.email if request.user.is_authenticated else 'world'
     return HttpResponse("Hello, " + name + ". You're at the Action Phase index.")

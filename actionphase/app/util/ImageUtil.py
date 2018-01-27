91bbe79cb5d596e54d7f8a21e31df1f5529362af
Jake Houser <jakob.houser@gmail.com>
2018-01-27 13:14:31 +0100

    Update image upload to field

--- actionphase/app/util/ImageUtil.py
+++ actionphase/app/util/ImageUtil.py
@@ -4,18 +4,15 @@ from uuid import uuid4
 
 class ImageUtil:
     @classmethod
-    def handle_image_upload(cls, path):
+    def character_directory_path(cls, instance, filename):
         # Taken from: https://stackoverflow.com/questions/15140942/django-imagefield-change-file-name-on-upload
-        def wrapper(instance, filename):
-            # get filename
-            if instance.pk:
-                filename = '{}'.format(instance.pk)
-            else:
-                # set filename as random string
-                filename = '{}'.format(uuid4().hex)
-            if hasattr(instance, 'image_version'):
-                instance.image_version = time.time()
-            # return the whole path to the file
-            return os.path.join(path, filename)
-
-        return wrapper
+        # get filename
+        if instance.pk:
+            filename = '{}'.format(instance.pk)
+        else:
+            # set filename as random string
+            filename = '{}'.format(uuid4().hex)
+        if hasattr(instance, 'image_version'):
+            instance.image_version = time.time()
+        # return the whole path to the file
+        return os.path.join('characters', filename)

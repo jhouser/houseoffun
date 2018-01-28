f6c47b7b70ed5af989eeca34a3a098fcdc52c425
Jake Houser <jakob.houser@gmail.com>
2018-01-28 12:35:40 +0100

    Add test to prevent double signup

--- actionphase/app/tests/integration/games/TestSignups.py
+++ actionphase/app/tests/integration/games/TestSignups.py
@@ -40,6 +40,16 @@ class SignupsTest(TestCase):
         signup = GameSignup.objects.filter(user=self.game_master, game=self.game).first()
         self.assertIsNone(signup)
 
+    def test_double_signup_failure(self):
+        # A user shouldn't be able to sign up for the same game twice
+        GameSignup.objects.create(user=self.user, game=self.game)
+        request = self.factory.get('/games/signup/')
+        request.user = self.user
+        response = game_signup(request, self.game.id)
+        self.assertEqual(response.status_code, 302)
+        signups = GameSignup.objects.filter(user=self.user, game=self.game)
+        self.assertEqual(1, len(signups))
+
     def test_game_withdraw(self):
         signup = GameSignup.objects.create(user=self.user, game=self.game)
         signup_id = signup.id

41718a5696a0847c48741523e0f1892036cf8914
Jake Houser <jakob.houser@gmail.com>
2018-01-29 20:45:17 +0100

    Redo start_server script to fix issues

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 docker-compose run --rm web pip install -r requirements.txt && \
-docker-compose run --rm web npm install && \
-docker-compose run --rm web python manage.py migrate && \
-docker-compose run --rm web python manage.py collectstatic --noinput && \
-docker-compose up -d
+docker-compose up -d && \
+docker exec -it code_web_1 npm install && \
+docker exec -it code_web_1  python manage.py migrate && \
+docker exec -it code_web_1  python manage.py collectstatic --noinput

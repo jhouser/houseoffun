09ef92dc456511942f2f74813af2a2b7a2eb47b8
Jake Houser <jakob.houser@gmail.com>
2018-01-29 22:38:30 +0100

    Suppress NPM output

--- aws-scripts/start_server.sh
+++ aws-scripts/start_server.sh
@@ -4,6 +4,6 @@ mv actionphase/.env.example actionphase/.env
 docker-compose build
 docker-compose run --rm web pip install -r requirements.txt >/dev/null
 docker-compose up -d
-docker exec -it code_web_1 npm install
+docker exec -it code_web_1 npm install --silent
 docker exec -it code_web_1  python manage.py migrate
 docker exec -it code_web_1  python manage.py collectstatic --noinput

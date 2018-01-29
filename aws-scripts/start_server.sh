d71c963fb1d193033ca6713585761882a3f0f776
Jake Houser <jakob.houser@gmail.com>
2018-01-30 00:09:51 +0100

    Remove interactivity from docker exec

--- aws-scripts/start_server.sh
+++ aws-scripts/start_server.sh
@@ -4,6 +4,6 @@ mv actionphase/.env.example actionphase/.env
 docker-compose build
 docker-compose run --rm web pip install -r requirements.txt
 docker-compose up -d
-docker exec -it code_web_1 npm install --silent
-docker exec -it code_web_1  python manage.py migrate
-docker exec -it code_web_1  python manage.py collectstatic --noinput
+docker exec code_web_1 npm install --silent
+docker exec code_web_1 python manage.py migrate
+docker exec code_web_1 python manage.py collectstatic --noinput

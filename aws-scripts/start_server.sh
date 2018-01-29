4c5e26dc4b0cc5bf5e3c9f9eb905e378045ccfe2
Jake Houser <jakob.houser@gmail.com>
2018-01-30 00:22:24 +0100

    Add a sleep to ensure containers start

--- aws-scripts/start_server.sh
+++ aws-scripts/start_server.sh
@@ -4,6 +4,7 @@ mv actionphase/.env.example actionphase/.env
 docker-compose build
 docker-compose run --rm web pip install -r requirements.txt
 docker-compose up -d
+sleep 15
 docker exec code_web_1 npm install --silent
 docker exec code_web_1 python manage.py migrate
 docker exec code_web_1 python manage.py collectstatic --noinput

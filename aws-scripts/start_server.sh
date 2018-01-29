07535391b989be509f2e4666435f4e70767265b2
Jake Houser <jakob.houser@gmail.com>
2018-01-29 23:32:13 +0100

    Move dev null pipes outside of dockerfile

--- aws-scripts/start_server.sh
+++ aws-scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /code
 mv actionphase/.env.example actionphase/.env
-docker-compose build
+docker-compose build >/dev/null
 docker-compose run --rm web pip install -r requirements.txt >/dev/null
 docker-compose up -d
 docker exec -it code_web_1 npm install --silent

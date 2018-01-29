9f38a1022ff1f5746e0e0ef05fbc2b1821072b14
Jake Houser <jakob.houser@gmail.com>
2018-01-29 23:45:35 +0100

    Redirect more stuff to /dev/null

--- aws-scripts/start_server.sh
+++ aws-scripts/start_server.sh
@@ -3,7 +3,7 @@ cd /code
 mv actionphase/.env.example actionphase/.env
 docker-compose build &>/dev/null
 docker-compose run --rm web pip install -r requirements.txt &>/dev/null
-docker-compose up -d &>/dev/null
-docker exec -it code_web_1 npm install --silent &>/dev/null
-docker exec -it code_web_1  python manage.py migrate &>/dev/null
-docker exec -it code_web_1  python manage.py collectstatic --noinput &>/dev/null
+docker-compose up -d
+docker exec -it code_web_1 npm install --silent
+docker exec -it code_web_1  python manage.py migrate
+docker exec -it code_web_1  python manage.py collectstatic --noinput

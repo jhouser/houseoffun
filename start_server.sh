25681f5df3c1ceae3c4c651b86836b9dc2d985d0
Jake Houser <jakob.houser@gmail.com>
2018-02-03 11:38:56 +0100

    Fix docker config

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 docker-compose run --rm web pip install -r requirements.txt && \
 docker-compose up -d && \
+sleep 5 && \
 docker exec code_web_1 npm install && \
 docker exec code_web_1 python manage.py migrate && \
 docker exec code_web_1 python manage.py collectstatic --noinput

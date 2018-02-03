bc2cc6e465194684485ff30591de9221cbd633c2
Jake Houser <jakob.houser@gmail.com>
2018-02-03 12:55:07 +0100

    Add line to run server just to make sure it's up

--- start_server.sh
+++ start_server.sh
@@ -4,3 +4,4 @@ docker exec code_web_1 pip install -r requirements.txt >/dev/null
 docker exec code_web_1 npm install
 docker exec code_web_1 python manage.py migrate
 docker exec code_web_1 python manage.py collectstatic --noinput
+docker exec code_web_1 python manage.py runserver 0.0.0.0:8000

2cf3342e44996851b01e80b9ac9537c4deda8d44
Jake Houser <jakob.houser@gmail.com>
2018-02-10 11:50:47 +0100

    Fix typos in test URL

--- actionphase/app/tests/functional/plugins/TestThreads.py
+++ actionphase/app/tests/functional/plugins/TestThreads.py
@@ -17,5 +17,5 @@ class ThreadsTest(BaseStaticLiveServerTestCase):
 
     def test_thread_view(self):
         self.authenticate()
-        self.selenium.get('%s%s%s' % (self.live_server_url, '/thrads/', url.REPLYABLE_THREAD_ID))
+        self.selenium.get('%s%s%s' % (self.live_server_url, '/threads/view/', url.REPLYABLE_THREAD_ID))
         self.selenium.find_element_by_id(css.THREAD_CONTENT_ID)

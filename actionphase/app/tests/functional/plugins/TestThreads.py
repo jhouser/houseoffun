9ce456c6ea2abe2bfadaad8d357f65142f3d9474
Jake Houser <jakob.houser@gmail.com>
2018-02-10 12:03:53 +0100

    Add a wait for VueJS to load

--- actionphase/app/tests/functional/plugins/TestThreads.py
+++ actionphase/app/tests/functional/plugins/TestThreads.py
@@ -24,6 +24,7 @@ class ThreadsTest(BaseStaticLiveServerTestCase):
     def test_thread_comment(self):
         self.authenticate()
         self.selenium.get('%s%s%s' % (self.live_server_url, '/threads/view/', url.REPLYABLE_THREAD_ID))
+        self.wait.until(ec.presence_of_element_located((By.CLASS_NAME, css.THREAD_SUBMIT_BUTTON_CLASS)))
         reply_text = TestHelper.random_string(32)
         text_input = self.selenium.find_element_by_name("text")
         text_input.send_keys(reply_text)

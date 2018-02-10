0b00d5340fa17202d73851256cb8463980b86dbb
Jake Houser <jakob.houser@gmail.com>
2018-02-10 13:16:16 +0100

    Revert "Revert "Remove debug statements""
    This reverts commit 9742c44e6d1c244132476dcf0a63a4413c0e941f.

--- actionphase/app/tests/functional/plugins/TestThreads.py
+++ actionphase/app/tests/functional/plugins/TestThreads.py
@@ -24,12 +24,6 @@ class ThreadsTest(BaseStaticLiveServerTestCase):
     def test_thread_comment(self):
         self.authenticate()
         self.selenium.get('%s%s%s' % (self.live_server_url, '/threads/view/', url.REPLYABLE_THREAD_ID))
-        page = self.selenium.find_elements(By.TAG_NAME, 'script')
-        for i in page:
-            self.selenium.get((i.get_attribute('src')))
-            print(self.selenium.page_source)
-        exit()
-
         self.wait.until(ec.element_to_be_clickable((By.CLASS_NAME, css.THREAD_SUBMIT_BUTTON_CLASS)))
         reply_text = TestHelper.random_string(32)
         text_input = self.selenium.find_element_by_name("text")

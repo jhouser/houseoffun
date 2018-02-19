01327aa21070e0ed164fd52e9850733486fd96c8
Jake Houser <jakob.houser@gmail.com>
2018-02-19 13:50:19 +0100

    Add boilerplate tutorial react page with login

---
+++ frontend/src/actions.js
@@ -0,0 +1,98 @@
+// There are three possible states for our login
+// process and we need actions for each of them
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+function requestLogin(creds) {
+  return {
+    type: LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  }
+}
+
+function receiveLogin(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    token: user.token
+  }
+}
+
+function loginError(message) {
+  return {
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  }
+}
+
+// Three possible states for our logout process as well.
+// Since we are using JWTs, we just need to remove the token
+// from localStorage. These actions are more useful if we
+// were calling the API to log the user out
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
+
+function requestLogout() {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true
+  }
+}
+
+function receiveLogout() {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false
+  }
+}
+
+// Calls the API to get a token and
+// dispatches actions along the way
+export function loginUser(creds) {
+
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+    body: `username=${creds.username}&password=${creds.password}`
+  }
+
+  return dispatch => {
+    // We dispatch requestLogin to kickoff the call to the API
+    dispatch(requestLogin(creds))
+
+    return fetch('http://192.168.99.100:8000/rest-auth/login/', config)
+      .then(response =>
+        response.json().then(user => ({ user, response }))
+            ).then(({ user, response }) =>  {
+        if (!response.ok) {
+          // If there was a problem, we want to
+          // dispatch the error condition
+          dispatch(loginError(user.message))
+          return Promise.reject(user)
+        } else {
+          // If login was successful, set the token in local storage
+          localStorage.setItem('token', user.token)
+          // Dispatch the success action
+          dispatch(receiveLogin(user))
+        }
+      }).catch(err => console.log("Error: ", err))
+  }
+}
+
+// Logs the user out
+export function logoutUser() {
+  return dispatch => {
+    dispatch(requestLogout())
+    localStorage.removeItem('token')
+    dispatch(receiveLogout())
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import quotesApp from './reducers'
+import thunkMiddleware from 'redux-thunk'
+//import api from './middleware/api'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
+
+let store = createStoreWithMiddleware(quotesApp)
+
+let rootElement = document.getElementById('root')
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)

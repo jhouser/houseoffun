ecfa5f0f615d01cdb34a8982655ee2b87c6be35a
Jake Houser <jakob.houser@gmail.com>
2018-02-24 12:11:50 +0100

    Remove default code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {store, persistor} from "./configureStore";
+import LayoutContainer from "./containers/LayoutContainer";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <LayoutContainer />
+        </PersistGate>
+    </Provider>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import { store, persistor } from './configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>
+    <App />
     , document.getElementById('root'));
 registerServiceWorker();

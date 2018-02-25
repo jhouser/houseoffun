8f013361a57a19934ad45e6587ad334c6ce7a748
Jake Houser <jakob.houser@gmail.com>
2018-02-25 21:09:40 +0100

    Create login form

---
+++ frontend/src/scenes/Home/components/LoginForm/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Link } from "react-router-dom";
+import './index.scss';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div className="login-form">
+                <form>
+                    <div className="login-form__item">
+                        <input type="email" id="email" placeholder="Email" />
+                    </div>
+                    <div className="login-form__item">
+                        <input type="password" id="password" placeholder="Password" />
+                    </div>
+                    <button type="submit" className="login-form__submit-button">Login</button>
+                    <div className="login-form__links">
+                        <Link className="login-form__link" to="/register">Need an account?</Link><br />
+                        <Link className="login-form__link" to="/register">Forgot your password?</Link>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
--- frontend/src/scenes/Home/index.js
+++ frontend/src/scenes/Home/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route } from "react-router-dom";
 import Splash from './components/Splash';
+import LoginForm from './components/LoginForm';
 import './index.scss';
 
 class Home extends Component {
@@ -8,6 +9,7 @@ class Home extends Component {
         return (
             <div className="home">
                 <Route exact path="/" component={Splash} />
+                <Route path="/login" component={LoginForm} />
             </div>
         );
     }

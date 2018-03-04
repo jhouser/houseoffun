d156f7b1689c9a55e9d48e555d0dea1b1ef880c5
Jake Houser <jakob.houser@gmail.com>
2018-03-04 10:50:22 +0100

    Make login form functional

--- frontend/src/scenes/Home/components/LoginForm/index.js
+++ frontend/src/scenes/Home/components/LoginForm/index.js
@@ -1,25 +1,45 @@
 import React, {Component} from 'react';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import PropTypes from 'prop-types'
 import './index.scss';
 
-const LoginForm = ({ onSubmit }) => (
-    <div className="login-form">
-        <form onSubmit={ e => {e.preventDefault(); onSubmit();}}>
-            <div className="login-form__item">
-                <input type="text" name="username" id="username" placeholder="Username" />
-            </div>
-            <div className="login-form__item">
-                <input type="password" name="password" id="password" placeholder="Password" />
-            </div>
-            <button type="submit" className="login-form__submit-button">Login</button>
-            <div className="login-form__links">
-                <Link className="login-form__link" to="/">Need an account?</Link><br />
-                <Link className="login-form__link" to="/">Forgot your password?</Link>
-            </div>
-        </form>
-    </div>
-);
+class LoginForm extends Component {
+    state = {
+        username: '',
+        password: ''
+    };
+    handleInputChange = (event) => {
+        const target = event.target,
+            value = target.type ===
+            'checkbox' ? target.checked : target.value,
+            name = target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.username, this.state.password)
+    };
+
+    render() {
+        return <div className="login-form">
+            <form onSubmit={this.onSubmit}>
+                <div className="login-form__item">
+                    <input onChange={this.handleInputChange} type="text" name="username" id="username" placeholder="Username"/>
+                </div>
+                <div className="login-form__item">
+                    <input onChange={this.handleInputChange} type="password" name="password" id="password" placeholder="Password"/>
+                </div>
+                <button type="submit" className="login-form__submit-button">Login</button>
+                <div className="login-form__links">
+                    <Link className="login-form__link" to="/">Need an account?</Link><br/>
+                    <Link className="login-form__link" to="/">Forgot your password?</Link>
+                </div>
+            </form>
+        </div>
+    }
+}
 
 LoginForm.propTypes = {
     onSubmit: PropTypes.func.isRequired,
--- frontend/src/scenes/Home/containers/Login/index.js
+++ frontend/src/scenes/Home/containers/Login/index.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
-import { Link } from "react-router-dom";
-import { Redirect } from 'react-router'
-import { connect } from 'react-redux'
-import {login} from  '../../../../actions/auth'
+import {Link} from "react-router-dom";
+import {Redirect} from 'react-router'
+import {connect} from 'react-redux'
+import {login} from '../../../../actions/auth'
 import {authErrors, isAuthenticated} from '../../../../util/auth'
 import LoginForm from '../../components/LoginForm'
 
 const mapStateToProps = state => ({
-  errors: authErrors(state),
-  isAuthenticated: isAuthenticated(state)
+    errors: authErrors(state),
+    isAuthenticated: isAuthenticated(state)
 });
 const mapDispatchToProps = dispatch => ({
-  onSubmit: (username, password) => {
-    dispatch(login(username, password))
-  }
+    onSubmit: (username, password) => {
+        dispatch(login(username, password))
+    }
 });
 
 const Login = connect(mapStateToProps, mapDispatchToProps)(LoginForm);

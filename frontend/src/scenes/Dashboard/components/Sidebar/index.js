30e0bb6f14068535a907c1b92b5c2ba6bf23a810
Jake Houser <jakob.houser@gmail.com>
2018-03-10 10:48:28 +0100

    Add links to Sidebar

--- frontend/src/scenes/Dashboard/components/Sidebar/index.js
+++ frontend/src/scenes/Dashboard/components/Sidebar/index.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 import './index.scss';
 
 class Sidebar extends Component {
     render() {
-        return <div className="sidebar">Sidebar</div>
+        return <div className="sidebar">
+            <div className="sidebar__title">
+                Sidebar
+            </div>
+            <div className="sidebar__links">
+                <ul>
+                    <li><Link to="/">Dashboard</Link></li>
+                    <li><Link to="/games">Games</Link></li>
+                </ul>
+            </div>
+        </div>
     }
 }
 

4e1b938cd683738cc2201473e44e4c893ff23e04
Jake Houser <jakob.houser@gmail.com>
2018-03-10 10:54:54 +0100

    Add components for DashboardContent and Games

--- frontend/src/scenes/Dashboard/components/Content/index.js
+++ frontend/src/scenes/Dashboard/components/Content/index.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
+import PrivateRoute from '../../../../containers/PrivateRoute';
 import './index.scss';
+import DashboardContent from "../../scenes/DashboardContent";
+import Games from "../../scenes/Games";
 
 class Content extends Component {
     render() {
-        return <div className="content">Content</div>
+        return <div className="content">
+            <Switch>
+                <PrivateRoute exact path="/" component={DashboardContent}/>
+                <PrivateRoute path="/games" component={Games}/>
+            </Switch>
+        </div>
     }
 }
 
---
+++ frontend/src/scenes/Dashboard/scenes/DashboardContent/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class DashboardContent extends Component {
+    render() {
+        return <div className="dashboard-content">Content</div>
+    }
+}
+
+export default DashboardContent;

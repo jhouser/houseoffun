4aef852123ca156cb3bf68d77f8f4f6a207fed6d
Jake Houser <jakob.houser@gmail.com>
2018-03-10 10:21:52 +0100

    Update imports

--- frontend/src/scenes/Home/index.js
+++ frontend/src/scenes/Home/index.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
 import Splash from './components/Splash';
 import Login from './containers/Login';
 import Register from './containers/Register';
 import './index.scss';
 import {isAuthenticated} from "../../util/auth";
+import {connect} from 'react-redux';
 
 const Home = (props) => {
     if (props.isAuthenticated) {

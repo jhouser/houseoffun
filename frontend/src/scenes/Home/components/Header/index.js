6832aac911ee95e01bfd8243cd590bebfd95d144
Jake Houser <jakob.houser@gmail.com>
2018-03-11 14:52:48 +0100

    Add navbar

--- frontend/src/scenes/Home/components/Header/index.js
+++ frontend/src/scenes/Home/components/Header/index.js
@@ -1,9 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import './index.scss';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
     render() {
-        return <div className="header">Action Phase</div>
+        return <Navbar color="faded" light expand="md" className="header">
+            <NavbarBrand>Action Phase</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}/>
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink>Games</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>Games</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Test!
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem>
+                                Profile
+                            </DropdownItem>
+                            <DropdownItem>
+                                Settings
+                            </DropdownItem>
+                            <DropdownItem divider/>
+                            <DropdownItem>
+                                Logout
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+            </Collapse>
+        </Navbar>
     }
 }
 

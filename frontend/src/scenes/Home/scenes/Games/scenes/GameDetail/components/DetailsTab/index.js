5d3c73159c1f41be830413a4f34e4e77a72c51ab
Jake Houser <jakob.houser@gmail.com>
2018-03-29 20:25:27 +0200

    Clean up details tab

--- frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/DetailsTab/index.js
+++ frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/DetailsTab/index.js
@@ -8,13 +8,15 @@ class DetailsTab extends Component {
             <Col md="12">
                 <Row>
                     <Col sm="12" md="6">
+                        <div className="gameDetail__attribute_label">Details</div>
                         <div className="gameDetail__attribute gameDetail__description">
-                            Description: {this.props.description}
+                            {this.props.description}
                         </div>
                     </Col>
                     <Col sm="12" md="6">
+                        <div className="gameDetail__attribute_label">Character Guidelines</div>
                         <div className="gameDetail__attribute gameDetail__character_guidelines">
-                            Character Guidelines: {this.props.character_guidelines}
+                            {this.props.character_guidelines}
                         </div>
                     </Col>
                 </Row>

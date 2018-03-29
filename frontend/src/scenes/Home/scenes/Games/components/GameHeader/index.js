987b5a7e36ac68a87559d0fa59d4ec1de48f32fc
Jake Houser <jakob.houser@gmail.com>
2018-03-29 20:17:38 +0200

    Add styling to GameHeader

--- frontend/src/scenes/Home/scenes/Games/components/GameHeader/index.js
+++ frontend/src/scenes/Home/scenes/Games/components/GameHeader/index.js
@@ -4,7 +4,7 @@ import {TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, Breadcrumb, Breadc
 
 class GameHeader extends Component {
     render() {
-        return <div>
+        return <div className="gameHeader">
             <Row>
                 <Col sm="12">
                     <h2>{this.props.name} ({this.props.abbreviation})</h2>
@@ -13,7 +13,7 @@ class GameHeader extends Component {
 
             <Row>
                 <Col sm="12">
-                    <div className="gameDetail__attribute gameDetail__game_master">
+                    <div className="gameHeader__attribute gameHeader__game_master">
                         by {this.props.game_master ? this.props.game_master.username : ''}
                     </div>
                 </Col>
@@ -22,7 +22,7 @@ class GameHeader extends Component {
             <Row>
                 <Col sm="12">
                     <div
-                        className={`gameDetail__attribute gameDetail__status gameDetail__status-${this.props.get_status_display}`}>
+                        className={`gameHeader__attribute gameHeader__status gameHeader__status-${this.props.get_status_display}`}>
                         {this.props.get_status_display}
                     </div>
                 </Col>

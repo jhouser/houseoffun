72c3f0e7e28fca4ad673f2b7f27372eb5887d8e5
Jake Houser <jakob.houser@gmail.com>
2018-03-30 11:30:10 +0200

    Update docker-compose version

--- aws-scripts/install_dependencies.sh
+++ aws-scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 yum update
 yum install -y docker
 usermod -a -G docker ec2-user
-curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | tee /usr/local/bin/docker-compose > /dev/null
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` | tee /usr/local/bin/docker-compose > /dev/null
 chmod +x /usr/local/bin/docker-compose
 service docker start
 chkconfig docker on

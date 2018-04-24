2ecee93def7b770cb63a08ccb376be46d6030ec4
Jake Houser <jakob.houser@gmail.com>
2018-04-24 22:06:36 +0200

    Revert "Try running tests from project root"
    This reverts commit 6b2301d4c8ff690d6de29fdbe1c2763fd6bb6186.

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -5,7 +5,5 @@ if [ "$TO_TEST" = "BACKEND" ]; then
     pip install -r backend/requirements.txt
 fi
 if [ "$TO_TEST" = "FRONTEND" ]; then
-    cp frontend/package.json ./package.json
-    npm install
     cd frontend && npm install
 fi

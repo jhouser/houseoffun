bc572873a4aa73d50414255d2273bb906a25a3cb
Jake Houser <jakob.houser@gmail.com>
2018-04-24 21:24:57 +0200

    Remove global jest install

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -5,6 +5,5 @@ if [ "$TO_TEST" = "BACKEND" ]; then
     pip install -r backend/requirements.txt
 fi
 if [ "$TO_TEST" = "FRONTEND" ]; then
-    npm install -g jest
     cd frontend && npm install
 fi

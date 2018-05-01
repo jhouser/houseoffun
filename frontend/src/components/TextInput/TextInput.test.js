4c9b719eb0fc268e5cf9139932fd2e04042d1cb3
Jake Houser <jakob.houser@gmail.com>
2018-05-01 10:42:21 +0200

    Rewrite test to have props and setup

--- frontend/src/components/TextInput/TextInput.test.js
+++ frontend/src/components/TextInput/TextInput.test.js
@@ -1,11 +1,28 @@
 import React from "react";
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 import {TextInput} from ".";
 
 describe("TextInput", () => {
+
+    let props;
+    let mountedTextInput;
+    const textInput = () => {
+        if (!mountedTextInput) {
+            mountedTextInput = mount(
+                <TextInput {...props} />
+            );
+        }
+        return mountedTextInput;
+    };
+
+    beforeEach(() => {
+        props = {};
+        mountedTextInput = undefined;
+    });
+
     it("always renders a FormGroup", () => {
-        const wrapper = shallow(<TextInput />);
-        expect(wrapper.find("FormGroup").length).toBe(1);
+        const formGroup = textInput().find("FormGroup");
+        expect(formGroup.length).toBe(1);
     });
 });
 

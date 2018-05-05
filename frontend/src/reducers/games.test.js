5e19a49fa8f821e412248e2cc328bfdb1e8deeed
Jake Houser <jakob.houser@gmail.com>
2018-05-05 13:25:52 +0200

    Add tests for games functions

--- frontend/src/reducers/games.test.js
+++ frontend/src/reducers/games.test.js
@@ -1,11 +1,55 @@
 import {Reducer} from 'redux-testkit';
-import gameReducer from './games';
-import * as games from '../actions/games';
+import gameReducer, {games, game, isGameMaster} from './games';
+import * as gameActions from '../actions/games';
 
 const initialState = {
     game: {
         list: [],
         details: {}
+    }
+};
+
+const functionTestState = {
+    game: {
+        list: [
+            {
+                name: "Test Game"
+            }
+        ],
+        details: {
+            name: "Test Game",
+            game_master: {
+                id: 1,
+                name: "Test User"
+            }
+        }
+    },
+    auth: {
+        access: {
+            user_id: 1
+        }
+    }
+};
+
+const notGmState = {
+    game: {
+        list: [
+            {
+                name: "Test Game"
+            }
+        ],
+        details: {
+            name: "Test Game",
+            game_master: {
+                id: 1,
+                name: "Test User"
+            }
+        }
+    },
+    auth: {
+        access: {
+            user_id: 2
+        }
     }
 };
 
@@ -25,9 +69,21 @@ describe('GameReducer', () => {
         Reducer(gameReducer).expect({type: 'NOT_EXISTING'}).toReturnState(initialState);
     });
     it('should return a list of games from the API on list success', () => {
-        Reducer(gameReducer).expect({type: games.GAME_LIST_SUCCESS, payload: requestPayload}).toReturnState(gameListSuccess);
+        Reducer(gameReducer).expect({type: gameActions.GAME_LIST_SUCCESS, payload: requestPayload}).toReturnState(gameListSuccess);
     });
     it('should return a game details from the API on detail success', () => {
-        Reducer(gameReducer).expect({type: games.GAME_DETAIL_SUCCESS, payload: requestPayload}).toReturnState(gameDetailSuccess);
+        Reducer(gameReducer).expect({type: gameActions.GAME_DETAIL_SUCCESS, payload: requestPayload}).toReturnState(gameDetailSuccess);
+    });
+    it('should export a function called "games" which returns the list of games from state', () => {
+        expect(games(functionTestState)).toBe(functionTestState.game.list);
+    });
+    it('should export a function called "game" which returns the game details from state', () => {
+        expect(game(functionTestState)).toBe(functionTestState.game.details);
+    });
+    it('should export a function called "isGameMaster" which returns true if the logged in user is the game master of the current game', () => {
+        expect(isGameMaster(functionTestState)).toBe(true);
+    });
+    it('should export a function called "isGameMaster" which returns false if the logged in user is not the game master of the current game', () => {
+        expect(isGameMaster(notGmState)).toBe(false);
     });
 });

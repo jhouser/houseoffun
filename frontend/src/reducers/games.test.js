a21781366770f60e9996e461570c0d78ad0e7bc3
Jake Houser <jakob.houser@gmail.com>
2018-05-05 13:31:24 +0200

    Refactor structure of function tests

--- frontend/src/reducers/games.test.js
+++ frontend/src/reducers/games.test.js
@@ -9,50 +9,6 @@ const initialState = {
     }
 };
 
-const functionTestState = {
-    game: {
-        list: [
-            {
-                name: "Test Game"
-            }
-        ],
-        details: {
-            name: "Test Game",
-            game_master: {
-                id: 1,
-                name: "Test User"
-            }
-        }
-    },
-    auth: {
-        access: {
-            user_id: 1
-        }
-    }
-};
-
-const notGmState = {
-    game: {
-        list: [
-            {
-                name: "Test Game"
-            }
-        ],
-        details: {
-            name: "Test Game",
-            game_master: {
-                id: 1,
-                name: "Test User"
-            }
-        }
-    },
-    auth: {
-        access: {
-            user_id: 2
-        }
-    }
-};
-
 const requestPayload = "PAYLOAD";
 const gameListSuccess = {
     list: requestPayload
@@ -74,16 +30,51 @@ describe('GameReducer', () => {
     it('should return a game details from the API on detail success', () => {
         Reducer(gameReducer).expect({type: gameActions.GAME_DETAIL_SUCCESS, payload: requestPayload}).toReturnState(gameDetailSuccess);
     });
-    it('should export a function called "games" which returns the list of games from state', () => {
-        expect(games(functionTestState)).toBe(functionTestState.game.list);
-    });
-    it('should export a function called "game" which returns the game details from state', () => {
-        expect(game(functionTestState)).toBe(functionTestState.game.details);
-    });
-    it('should export a function called "isGameMaster" which returns true if the logged in user is the game master of the current game', () => {
-        expect(isGameMaster(functionTestState)).toBe(true);
-    });
-    it('should export a function called "isGameMaster" which returns false if the logged in user is not the game master of the current game', () => {
-        expect(isGameMaster(notGmState)).toBe(false);
+    describe('the exported functions', () => {
+        let functionTestState;
+        beforeEach(() => {
+             functionTestState = {
+                game: {
+                    list: [
+                        {
+                            name: "Test Game"
+                        }
+                    ],
+                    details: {
+                        name: "Test Game",
+                        game_master: {
+                            id: 1,
+                            name: "Test User"
+                        }
+                    }
+                },
+                auth: {
+                    access: {
+                        user_id: 1
+                    }
+                }
+            };
+        });
+        it('should include a function called "games" which returns the list of games from state', () => {
+            expect(games(functionTestState)).toBe(functionTestState.game.list);
+        });
+        it('should include a function called "game" which returns the game details from state', () => {
+            expect(game(functionTestState)).toBe(functionTestState.game.details);
+        });
+        describe('the isGameMaster function', () => {
+            describe('when user_id and game_master match', () => {
+                it('should return true', () => {
+                    expect(isGameMaster(functionTestState)).toBe(true);
+                });
+            });
+            describe('when user_id and game_master do not match', () => {
+                beforeEach(() => {
+                    functionTestState.auth.access.user_id = 2;
+                });
+                it('should return false', () => {
+                    expect(isGameMaster(functionTestState)).toBe(false);
+                });
+            });
+        });
     });
 });

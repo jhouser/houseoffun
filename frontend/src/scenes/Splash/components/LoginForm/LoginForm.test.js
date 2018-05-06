f57c1f141246b956002092796ce4ecf50d2f223a
Jake Houser <jakob.houser@gmail.com>
2018-05-06 12:31:28 +0200

    Add tests for errors on the login page

--- frontend/src/scenes/Splash/components/LoginForm/LoginForm.test.js
+++ frontend/src/scenes/Splash/components/LoginForm/LoginForm.test.js
@@ -35,4 +35,49 @@ describe("LoginForm", () => {
         const passwordInput = loginForm().find("TextInput[name='password']");
         expect(passwordInput.length).toBe(1);
     });
+    describe("when 'errors' is defined", () => {
+        describe("and there are non-field errors", () => {
+            beforeEach(() => {
+                props = {
+                    onSubmit: jest.fn(),
+                    errors: {
+                        non_field_errors: "Test Error Message"
+                    }
+                }
+            });
+            it("renders an Alert with the error message", () => {
+                const alert = loginForm().find("Alert");
+                expect(alert.length).toBe(1);
+                expect(alert.text()).toBe(props.errors.non_field_errors)
+            })
+        });
+        describe("and there are errors on the username field", () => {
+            beforeEach(() => {
+                props = {
+                    onSubmit: jest.fn(),
+                    errors: {
+                        username: "Test Error Message"
+                    }
+                }
+            });
+            it("renders an error on the username input", () => {
+                const usernameInput = loginForm().find("TextInput[name='username']");
+                expect(usernameInput.prop('error')).toBe(props.errors.username);
+            });
+        });
+        describe("and there are errors on the password field", () => {
+            beforeEach(() => {
+                props = {
+                    onSubmit: jest.fn(),
+                    errors: {
+                        password: "Test Error Message"
+                    }
+                }
+            });
+            it("renders an error on the password input", () => {
+                const passwordInput = loginForm().find("TextInput[name='password']");
+                expect(passwordInput.prop('error')).toBe(props.errors.password);
+            });
+        });
+    });
 });

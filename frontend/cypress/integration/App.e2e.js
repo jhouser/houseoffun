d3d131d5cb22ff2221d904ad04f6aec7b3898468
Jake Houser <jakob.houser@gmail.com>
2018-07-01 15:55:14 +0200

    Intentionally fail test to verify it works

--- frontend/cypress/integration/App.e2e.js
+++ frontend/cypress/integration/App.e2e.js
@@ -1,5 +1,6 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
-    cy.visit('/');
-  });
+describe('The Home Page', function () {
+    it('successfully loads', function () {
+        cy.visit('/');
+        cy.url().should('include', '/dashboard')
+    });
 });

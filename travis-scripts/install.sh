ded3f39e458ae6d22cd650156a4e89b306ae8a7d
Jake Houser <jakob.houser@gmail.com>
2018-07-01 15:07:08 +0200

    Clean up travis scripts

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ev
-pip install coveralls
 if [ "$TO_TEST" = "BACKEND" ]; then
     pip install -r backend/requirements.txt
     pip install codecov
 fi
 if [ "$TO_TEST" = "FRONTEND" ]; then
-    cd frontend && npm install && npm install -g cypress
+    cd frontend && npm ci && npm install -g cypress codecov
 fi

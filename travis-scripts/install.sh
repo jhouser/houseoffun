5a27f9c7d6b5d5f275751613b78aefcb95d4e4a5
Jake Houser <jakob.houser@gmail.com>
2018-07-01 13:21:57 +0200

    Update Travis to use Codecov

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -3,7 +3,8 @@ set -ev
 pip install coveralls
 if [ "$TO_TEST" = "BACKEND" ]; then
     pip install -r backend/requirements.txt
+    pip install codecov
 fi
 if [ "$TO_TEST" = "FRONTEND" ]; then
-    cd frontend && npm install
+    cd frontend && npm install && npm install -g codecov
 fi

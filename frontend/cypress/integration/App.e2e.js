8aea56c4c4e6ff77f14e12478f156f8cc3185845
Jake Houser <jakob.houser@gmail.com>
2018-07-01 15:58:32 +0200

    Fix the cypress test

--- frontend/cypress/integration/App.e2e.js
+++ frontend/cypress/integration/App.e2e.js
@@ -1,6 +1,6 @@
 describe('The Home Page', function () {
     it('successfully loads', function () {
         cy.visit('/');
-        cy.url().should('include', '/dashboard')
+        cy.url().should('include', '/home')
     });
 });

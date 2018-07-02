1d281f63f942e1f54d41349d0fb862928687104e
Jake Houser <jakob.houser@gmail.com>
2018-07-02 20:45:31 +0200

    Add test for login/registration links

--- frontend/cypress/integration/App.e2e.js
+++ frontend/cypress/integration/App.e2e.js
@@ -3,4 +3,9 @@ describe('The Home Page', function () {
         cy.visit('/');
         cy.url().should('include', '/home')
     });
+    it('has links to the login and registration pages', function () {
+        cy.visit('/');
+        cy.get('a[href="/home/login"]').should('exist');
+        cy.get('a[href="/home/register"]').should('exist');
+    });
 });

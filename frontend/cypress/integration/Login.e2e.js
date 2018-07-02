f416fae46c1f9674862d0e0bbcf5a7ede72bfc2a
Jake Houser <jakob.houser@gmail.com>
2018-07-02 20:51:52 +0200

    Update login test to actually do something (but fail)

--- frontend/cypress/integration/Login.e2e.js
+++ frontend/cypress/integration/Login.e2e.js
@@ -5,7 +5,11 @@ describe('The login page', function () {
         cy.get('[data-testid="userLoginForm"]').should('exist');
     });
     it('allows a user to login', function () {
+        const username = 'test_user';
+        const password = 'test_password';
         cy.visit('/home/login');
-        cy.url().should('include', '/home/login');
+        cy.get('input[name=username]').type(username);
+        cy.get('input[name=password]').type(`${password}{enter}`);
+        cy.get('[data-testid="logoutButton"]').should('exist');
     });
 });

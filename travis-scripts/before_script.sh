1b5b6edfd945d4e6e03cc43f5c482e5c9eae6e51
Jake Houser <jakob.houser@gmail.com>
2018-07-24 18:18:22 +0200

    Refactor tests to use global cypress

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -7,6 +7,7 @@ if [ "$BACKEND" = 1 ]; then
 fi
 
 if [ "$TESTS" = "e2e" ]; then
+    npm i -g cypress
     cd backend && ./load_fixtures.sh
     python backend/manage.py runserver 0.0.0.0:8000 &
     cd frontend && npm start -- --silent &

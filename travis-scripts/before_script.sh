f09de2ece9d64d4653c57ee0854091b05fee9c3a
Jake Houser <jakob.houser@gmail.com>
2018-07-24 18:49:22 +0200

    Add call to echo process list

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -14,5 +14,6 @@ if [ "$TESTS" = "e2e" ]; then
     npm i -g cypress
     cd backend && sh load_fixtures.sh && cd ..
     python backend/manage.py runserver 0.0.0.0:8000 &
+    ps aux
     cd frontend && npm start -- --silent &
 fi

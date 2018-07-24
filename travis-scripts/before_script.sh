6e6c01332239d112f2233e62b153212d21b28c87
Jake Houser <jakob.houser@gmail.com>
2018-07-24 18:29:09 +0200

    Change directory after script run

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$TESTS" = "e2e" ]; then
     npm i -g cypress
-    cd backend && sh load_fixtures.sh
+    cd backend && sh load_fixtures.sh && cd ..
     python backend/manage.py runserver 0.0.0.0:8000 &
     cd frontend && npm start -- --silent &
 fi

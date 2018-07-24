d8bae16785d3af42d44c51386c35dd8495b77e82
Jake Houser <jakob.houser@gmail.com>
2018-07-24 18:05:30 +0200

    Fix typo

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -7,7 +7,7 @@ if [ "$BACKEND" = 1 ]; then
 fi
 
 if [ "$TESTS" = "e2e" ]; then
-    cd frontend && python load_fixtures.sh
+    cd backend && python load_fixtures.sh
     python backend/manage.py runserver 0.0.0.0:8000 &
     cd frontend && npm start -- --silent &
 fi

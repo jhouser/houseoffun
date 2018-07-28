05f0e1da195e453d34b73c9317578a109b7a03b0
Jake Houser <jakob.houser@gmail.com>
2018-07-28 14:09:00 +0200

    Update installation process

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -7,3 +7,8 @@ fi
 if [ "$FRONTEND" = 1 ]; then
     npm -g i npm codecov && cd frontend && npm ci
 fi
+
+if [ "$TESTS" = "e2e" ]; then
+    export DISPLAY=':99.0'
+    Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+fi

5e0c8f7c856b195d86dfd7c5be254cefc5b2fa6a
Jake Houser <jakob.houser@gmail.com>
2018-07-28 12:32:23 +0200

    Remove curl call

--- travis-scripts/before_script.sh
+++ travis-scripts/before_script.sh
@@ -15,7 +15,4 @@ if [ "$TESTS" = "e2e" ]; then
     cd backend && sh load_fixtures.sh
     python manage.py runserver 0.0.0.0:8000 &
     cd ../frontend && npm start -- --silent &
-    # Wait for servers to load
-    sleep 10
-    curl -H "Content-Type: application/json" --request POST --data '{"email": "test_register@example.com", "username": "test_register_user", "password1": "test_register_password", "password2": "test_register_password"}' http://localhost:8000/api/auth/registration/
 fi

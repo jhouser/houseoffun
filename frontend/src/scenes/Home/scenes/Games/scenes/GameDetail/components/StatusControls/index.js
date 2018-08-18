bb054ed01603acee3b1b7c63f1823dff3e6faab9
Jake Houser <jakob.houser@gmail.com>
2018-08-18 11:44:21 +0200

    Dynamically render text of advance/revert buttons

--- frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/StatusControls/index.js
+++ frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/StatusControls/index.js
@@ -34,14 +34,14 @@ class StatusControls extends Component {
                     // Only show revert button if there is a 'prev' status defined
                     statuses[status].prev !== undefined ?
                         <Col sm="6">
-                            <Button color="secondary">Previous Status</Button>
+                            <Button color="secondary">Revert to {statuses[status].prev}</Button>
                         </Col> : ""
                 }
                 {
                     // Only show next button if there is a 'next' status defined
                     statuses[status].next !== undefined ?
                         <Col sm="6">
-                            <Button color="primary">Next Status</Button>
+                            <Button color="primary">Advance to {statuses[status].next}</Button>
                         </Col> : ""
                 }
             </Row>

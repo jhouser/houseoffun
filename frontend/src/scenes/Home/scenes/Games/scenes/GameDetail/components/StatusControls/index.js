470f9e571aa36663cbac595f373db1b13910bc1e
Jake Houser <jakob.houser@gmail.com>
2018-08-18 11:43:22 +0200

    Change logic for displaying buttons

--- frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/StatusControls/index.js
+++ frontend/src/scenes/Home/scenes/Games/scenes/GameDetail/components/StatusControls/index.js
@@ -3,28 +3,47 @@ import './index.scss';
 import {Row, Col, Button} from 'reactstrap';
 
 const statuses = {
-    'DR': 'Draft',
-    'RG': 'Recruiting',
-    'PD': 'Pending',
-    'RN': 'Running',
-    'FN': 'Finished'
+    'Draft': {
+        'prev': undefined,
+        'next': 'Recruiting'
+    },
+    'Recruiting': {
+        'prev': 'Draft',
+        'next': 'Pending'
+    },
+    'Pending': {
+        'prev': 'Recruiting',
+        'next': 'Running'
+    },
+    'Running': {
+        'prev': 'Pending',
+        'next': 'Finished'
+    },
+    'Finished': {
+        'prev': 'Running',
+        'next': 'Undefined'
+    },
 };
 
 class StatusControls extends Component {
     render() {
-        const status = this.props.status || statuses.DR;
+        const status = this.props.status || 'Draft';
         return <div className="gameHeader__status-controls">
             <Row>
                 {
-                    // Only show revert button if we're past the "Draft" status as we can't revert beyond that
-                    status !== statuses.DR ?
+                    // Only show revert button if there is a 'prev' status defined
+                    statuses[status].prev !== undefined ?
                         <Col sm="6">
                             <Button color="secondary">Previous Status</Button>
                         </Col> : ""
                 }
-                <Col sm="6">
-                    <Button color="primary">Next Status</Button>
-                </Col>
+                {
+                    // Only show next button if there is a 'next' status defined
+                    statuses[status].next !== undefined ?
+                        <Col sm="6">
+                            <Button color="primary">Next Status</Button>
+                        </Col> : ""
+                }
             </Row>
         </div>
     }
